d4de13b7a6cdd55759ff5b9876cfad253a454aa9
Koki Ito <kokiito0926@gmail.com>
2026-01-20 19:36:19 +0900

    feat: 🎸 コマンドライン引数の確認の処理を書いた
    コマンドライン引数でファイルのパスが指定されているのかを確認する。
    それから、そのファイルが実際に存在しているのかを確認する。

--- import.js
+++ import.js
@@ -1,12 +1,24 @@
-#!/usr/bin/env zx
+#!/usr/bin/env node
 
-import { argv } from "zx";
+import { fs, path, argv } from "zx";
 
 import { register } from "node:module";
 import { pathToFileURL } from "node:url";
 
 register("./hook.js", import.meta.url);
 
-const targetPath = path.resolve(process.cwd(), argv._[1]);
+// console.log(argv);
+
+if (!argv._[0]) {
+	process.exit(1);
+}
+
+const targetPath = path.resolve(process.cwd(), argv._[0]);
+// const targetPath = path.resolve(process.cwd(), argv._[1]);
+
+if (!fs.existsSync(targetPath)) {
+	process.exit(1);
+}
+
 await import(pathToFileURL(targetPath).href);
 // await import(argv._[1]);
